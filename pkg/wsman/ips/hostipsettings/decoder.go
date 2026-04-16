0968cbd66a06bbe4b9367785551cf80731d92c94
ShradhaGupta31 <shradha.gupta@intel.com>
2026-04-16 11:43:31 +0530

    feat: add missing wsman fetcher class function for IPS Classes
    - Add new package with service, types, decoder, and JSON/YAML marshalling
    - Add unit tests for the newly added package
    - List of Packages added -
    	- IPS_HostBootReason
    	- IPS_HostIPSettings
    	- IPS_IPv6PortSettings
    	- IPS_LANEndpoint
    	- IPS_ProvisioningRecordLog
    
    Signed-off-by: ShradhaGupta31 <shradha.gupta@intel.com>

---
+++ pkg/wsman/ips/hostipsettings/decoder.go
@@ -0,0 +1,10 @@
+/*********************************************************************
+ * Copyright (c) Intel Corporation 2026
+ * SPDX-License-Identifier: Apache-2.0
+ **********************************************************************/
+
+package hostipsettings
+
+const (
+	IPSHostIPSettings string = "IPS_HostIPSettings"
+)
--- pkg/wsman/ips/messages_test.go
+++ pkg/wsman/ips/messages_test.go
@@ -11,10 +11,15 @@ import (
 
 	"github.com/device-management-toolkit/go-wsman-messages/v2/pkg/wsman/ips/alarmclock"
 	"github.com/device-management-toolkit/go-wsman-messages/v2/pkg/wsman/ips/hostbasedsetup"
+	"github.com/device-management-toolkit/go-wsman-messages/v2/pkg/wsman/ips/hostbootreason"
+	"github.com/device-management-toolkit/go-wsman-messages/v2/pkg/wsman/ips/hostipsettings"
 	"github.com/device-management-toolkit/go-wsman-messages/v2/pkg/wsman/ips/http"
 	"github.com/device-management-toolkit/go-wsman-messages/v2/pkg/wsman/ips/ieee8021x"
+	"github.com/device-management-toolkit/go-wsman-messages/v2/pkg/wsman/ips/ipv6portsettings"
+	"github.com/device-management-toolkit/go-wsman-messages/v2/pkg/wsman/ips/lanendpoint"
 	"github.com/device-management-toolkit/go-wsman-messages/v2/pkg/wsman/ips/optin"
 	"github.com/device-management-toolkit/go-wsman-messages/v2/pkg/wsman/ips/power"
+	"github.com/device-management-toolkit/go-wsman-messages/v2/pkg/wsman/ips/provisioningrecordlog"
 	"github.com/device-management-toolkit/go-wsman-messages/v2/pkg/wsman/wsmantesting"
 )
 
@@ -32,6 +37,26 @@ func TestNewMessages(t *testing.T) {
 
 	if reflect.DeepEqual(m.HostBasedSetupService, hostbasedsetup.Service{}) {
 		t.Error("AuditLog is not initialized")
+	}
+
+	if reflect.DeepEqual(m.HostBootReason, hostbootreason.Service{}) {
+		t.Error("HostBootReason is not initialized")
+	}
+
+	if reflect.DeepEqual(m.HostIPSettings, hostipsettings.Settings{}) {
+		t.Error("HostIPSettings is not initialized")
+	}
+
+	if reflect.DeepEqual(m.IPv6PortSettings, ipv6portsettings.Settings{}) {
+		t.Error("IPv6PortSettings is not initialized")
+	}
+
+	if reflect.DeepEqual(m.LANEndpoint, lanendpoint.Endpoint{}) {
+		t.Error("LANEndpoint is not initialized")
+	}
+
+	if reflect.DeepEqual(m.ProvisioningRecordLog, provisioningrecordlog.Log{}) {
+		t.Error("ProvisioningRecordLog is not initialized")
 	}
 
 	if reflect.DeepEqual(m.AlarmClockOccurrence, alarmclock.Occurrence{}) {
